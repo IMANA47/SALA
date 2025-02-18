004916cb3743dffd1d8b6f15e9fde18f793f6e77
Nsengimana François <francknsengimana@gmail.com>
2025-02-18 13:08:39 +0100

    prepartion et mise en place de l'environnement pour le projet SALA

---
+++ js/index.js
@@ -0,0 +1,7 @@
+//DOM selection
+const list ='élement #list';
+const input ='élement #input';
+const add ='élement #add';
+const clear ='élement #clear';
+const url ='élement #url';
+const load ='élement #load';
